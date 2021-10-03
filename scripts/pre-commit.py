93ed0f1232c7a083aca5f2941e003a39816865aa
Queensferry <queensferry.me@gmail.com>
2021-10-03 11:59:48 +0800

    ci: improve pre-commit flake8 hook

--- scripts/pre-commit.py
+++ scripts/pre-commit.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
+import sys
 from pathlib import Path
 
 
 basedir = Path(__file__).parent.parent
 os.chdir(basedir)
-os.system(
-    "pip install -U arrow httpx hypothesis importlib-metadata pydantic"
-    "pytest pytest-asyncio starlette types-dataclasses"
-)
-os.system("mypy .")
+
+deps = {
+    "flake8": [
+        "darglint",
+        "flake8-bugbear",
+        "flake8-builtins",
+        "flake8-comprehensions",
+        "flake8-datetimez",
+        "flake8-debugger",
+        "flake8-docstrings",
+        "flake8-eradicate",
+        "flake8-print",
+        "flake8-too-many",
+        "pep8-naming",
+        "tryceratops",
+    ],
+    "mypy": [
+        "arrow",
+        "httpx",
+        "hypothesis",
+        "importlib-metadata",
+        "pydantic",
+        "pytest",
+        "pytest-asyncio",
+        "starlette",
+        "types-dataclasses",
+    ],
+}
+
+if sys.argv[1] == "flake8":
+    subprocess.call(["pip", "install", "-U", *deps["flake8"]])
+    subprocess.call(["flake8", "."])
+elif sys.argv[1] == "mypy":
+    subprocess.call(["pip", "install", "-U", *deps["mypy"]])
+    subprocess.call(["mypy", "."])
